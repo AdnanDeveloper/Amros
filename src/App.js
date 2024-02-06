5fbdf5004dc5cdcd6813648c0ea969750b2112f3
Adnan Moriswala <95620278+AdnanDeveloper@users.noreply.github.com>
2024-02-06 20:26:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './Components/Header';
 import './css/Header.css'
 import Home from './Components/Home';
 import Footer from './Components/Footer';
+import { Analytics } from '@vercel/analytics/react';
+
 function App() {
   return (
     <div className="App">
@@ -11,6 +13,7 @@ function App() {
         <Header />
         <Home />
         <Footer />
+        <Analytics />
         </div>
     </div>
   );
